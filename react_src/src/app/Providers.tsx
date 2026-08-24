45c0e32409c84f25ae63c3287adf119cd3ed0935
RicoTunes <ricolearn@gmail.com>
2026-08-23 19:18:15 -0700

    Dwene sync beam-site-ddfb9e75: update react_src/src/app/Providers.tsx

--- react_src/src/app/Providers.tsx
+++ react_src/src/app/Providers.tsx
@@ -2,13 +2,16 @@ import type { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 import { AuthProvider } from '@/contexts/AuthContext';
+import { CartProvider } from '@/contexts/CartContext';
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
+      <CartProvider>
       {children}
-    </AuthProvider>
+    </CartProvider>
+      </AuthProvider>
       </QueryClientProvider>
   );
 }
